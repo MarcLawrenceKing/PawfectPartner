27de8132ac54880bd5aa1ba3b81bbf2a1ac5f5e4
Alessa Estaras <115864832+alessaest@users.noreply.github.com>
2024-04-06 15:20:20 +0800

    commentssssss

--- src/com/app/view/ADPendingPetProfile.java
+++ src/com/app/view/ADPendingPetProfile.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.app.view;
 
 import com.app.controller.AdoptController;
@@ -10,13 +5,21 @@ import com.app.model.Account;
 import com.app.model.Pets;
 import java.util.Scanner;
 
-
+/*
+    this class represents the view for displaying pending pet profiles 
+*/
 public class ADPendingPetProfile {
+
+    /*
+        displays the profile of the pending pet and allows the user to cancel the adoption 
+        parameter petToView represents the pet whose profile is to be displayed
+        parameter account is used for the account accessing the pet profile
+    */
     public void adPendingPetProfile(Pets petToView, Account account){
         
-        Scanner sc = new Scanner(System.in);
-        AdoptController ac = new AdoptController();
-        AdoptAPet ap = new AdoptAPet();
+        Scanner sc = new Scanner(System.in); // initialize a scanner object for user input
+        AdoptController ac = new AdoptController(); // instantiate the controller for adoption operations
+        AdoptAPet ap = new AdoptAPet(); // instantiate the view for adopting a pet
         
         try {
             System.out.println("Name: " + petToView.getPet_name());
@@ -25,20 +28,20 @@ public class ADPendingPetProfile {
             System.out.println("Previous Status: " + petToView.getPet_prevState());
             System.out.println("Status: " + petToView.getPet_status());
 
-            if (petToView.getPet_status().equals("PENDING AD")) { 
+            if (petToView.getPet_status().equals("PENDING AD")) { // check if the pet status is pending adoption
                 System.out.print("Would you like to cancel this adoption? [Y/N]: ");
-                char yOrN = sc.next().charAt(0);
+                char yOrN = sc.next().charAt(0); // get user input for canceling adoption
         
-                if (Character.toLowerCase(yOrN) == 'y') {
-                    ac.ADUpdateToFOR_ADOPTION(petToView);
-                    ap.adoptAPet(account);
+                if (Character.toLowerCase(yOrN) == 'y') { // if user chooses to cancel adoption
+                    ac.ADUpdateToFOR_ADOPTION(petToView); // update pet status to "For Adoption"
+                    ap.adoptAPet(account); // display adoption view for the account
                 } 
             }
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e); // print any exception that might occur during execution
         }
 
-        sc.close();
+        sc.close(); // close the scanner to release system resources 
         
     }
 }
