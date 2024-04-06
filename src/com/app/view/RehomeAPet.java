2b5f25a51bd66b647b9c3476c12f0c00e0d1b907
Cassyyyzxc <164591955+Cassyyyzxc@users.noreply.github.com>
2024-04-07 00:49:12 +0800

    Update RehomeAPet.java

--- src/com/app/view/RehomeAPet.java
+++ src/com/app/view/RehomeAPet.java
@@ -1,15 +1,34 @@
-
+/**
+ * The RehomeAPet class is represents the view for showing rehome pet.
+ * It stores functionality for the users to view pending adoptions or pet types 
+ * and go back to the previous menu
+ *
+ * This class by the user interface to accelerate the rehoming process
+ * It interconnect with RHPendingAdoptions and RHPetTypes classes to provide choices for
+ * the users to view pending adoptions, pet types, or go back to the previous menu
+ *
+ * @author Alessa Estaras, Cassidy Fernandez, Kapangyarihan Randy, Marc King, Jhanna Llovit
+ *
+ * @version 04/07/2023
+ */
 package com.app.view;
-
 
 import com.app.model.Account;
 import com.app.model.Pets;
 import java.util.Scanner;
 
-
 public class RehomeAPet {
+    /**
+     * Showing choices for rehoming a pet and holds user input
+     * @param account The object represents the user account
+     */
     public void rehomeAPet(Account account) { 
         char choice;
+        
+        /*
+	     Scanner method named in to get the input from the user
+	     to be used all throughout the program
+	    */
         Scanner sc = new Scanner(System.in);
           RHPendingAdoptions ra = new RHPendingAdoptions();
           RHPetTypes rt = new RHPetTypes();
@@ -23,14 +42,14 @@ public class RehomeAPet {
             pet.setOwner_id(account.getUser_id()); // Sets pet's owner_id with users_id
             switch (choice) {
                 case '1':
-                    ra.rhPendingAdoption(pet,account);
+                    ra.rhPendingAdoption(pet,account); // View pending adoptions
                     continue;
                 case '2':
-                    rt.rhPetTypes(account, pet);
+                    rt.rhPetTypes(account, pet); // View pet types
                     continue;
                 case 'B':
                 sc.close();
-                    return;
+                    return; // Go back
             }
             System.out.println("");
         } while (true);
