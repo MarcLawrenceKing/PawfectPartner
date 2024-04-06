22a9c8a4b72061a3326f420617974539390ae19d
Alessa Estaras <115864832+alessaest@users.noreply.github.com>
2024-04-06 16:42:50 +0800

    commentsssssss

--- src/com/app/view/DisplayHomePage.java
+++ src/com/app/view/DisplayHomePage.java
@@ -1,13 +1,18 @@
-
 package com.app.view;
 
 import java.util.Scanner;
 
-
+/*
+    this class represents the interface for displaying the home page of the system
+*/
 public class DisplayHomePage {
+    /*
+        displays the home page and allows the user to create an account or log in
+        parameter sc is for the scanner object for user input
+    */
     public void displayHomePage (Scanner sc) {
-        CreateAccount ca = new CreateAccount();
-        LoginAccount la = new LoginAccount();
+        CreateAccount ca = new CreateAccount(); // instantiate the view for creating an account
+        LoginAccount la = new LoginAccount(); // instantiate the view for logging in
         
         int choice;
         do {
@@ -19,16 +24,16 @@ public class DisplayHomePage {
             
             System.out.println("\n[1] Create Account\n[2] Log In");
             System.out.print("Enter your choice => ");
-            choice = sc.nextInt();
-            sc.nextLine();
+            choice = sc.nextInt(); // get user input for the choice
+            sc.nextLine(); // consume newline character 
 
             switch (choice) {
-                case 1: ca.createAccount(); break;
-                case 2: la.loginAccount(); break;
+                case 1: ca.createAccount(); break; // display create account view
+                case 2: la.loginAccount(); break; // display login view 
                 default: 
-                  System.out.println("Inavlid input. Try again."); break;
+                  System.out.println("Inavlid input. Try again."); break; // prompt user for valid input 
             }
-        } while (choice != 1 && choice != 2);
+        } while (choice != 1 && choice != 2); // repeat until the user chooses to create an account or log in 
         
     }
 }
