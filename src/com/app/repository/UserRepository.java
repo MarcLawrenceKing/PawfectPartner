9f8091a627f1c8b3745c655df4a4f01111bc758f
Alessa Estaras <115864832+alessaest@users.noreply.github.com>
2024-04-06 14:48:47 +0800

    commentsssssssssss

--- src/com/app/repository/UserRepository.java
+++ src/com/app/repository/UserRepository.java
@@ -1,8 +1,27 @@
 package com.app.repository;
 import com.app.model.Account;
 
+/*
+    this interface defines the method for interacting with user account functionalities
+*/
 public interface UserRepository {
+
+    /*
+        creates a new user account
+        the parameter account represents the account to be created by the user
+    */
     void createAccount (Account account);
+
+    /*
+        logs in an existing user account
+        the parameter account is used to verify the details of the account to be log in
+    */
     void logInAccount (Account account);
+
+    /*
+        updates an existing user account based on the specified choice
+        the parameter account represents the account to be updated
+        the parameter choice is an integer representing the choice of the user
+    */
     public void updateAccount (Account account, int choice);
 }
