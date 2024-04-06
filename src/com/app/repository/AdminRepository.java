ce4b2ab54f4bc1277623c05e768ff8b74034726f
Alessa Estaras <115864832+alessaest@users.noreply.github.com>
2024-04-06 11:30:36 +0800

    commentsssssss

--- src/com/app/repository/AdminRepository.java
+++ src/com/app/repository/AdminRepository.java
@@ -1,14 +1,24 @@
 package com.app.repository;
 
+// importing the necessary packages and classes
 import java.util.ArrayList;
 
 import com.app.model.Account;
 import com.app.model.Pets;
 
+/*
+    this interface defines the methods for interacting 
+    with the admin functionalities
+*/
+
 public interface AdminRepository {
-    public void logInAccountAdmin (Account account);
-    public ArrayList<Account> ADMUsersTable();
-    public ArrayList<Pets> ADMPetsTable ();
+    // logs in an admin account 
+    public void logInAccountAdmin (Account account); // the method expects an object of the 'Account' thus admin will input a valid one (@admin)
+    // retrieves a table of admin users
+    public ArrayList<Account> ADMUsersTable(); // returns an ArrayList of account objects representing administrative users
+    // retrieves a table of administrative pet details
+    public ArrayList<Pets> ADMPetsTable (); // returns an ArrayList of pet objects representing administrative pets
+    // deletes archived pets in the administrative system
     public void ADMDeltePetsArchived ();
 
 }
