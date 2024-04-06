64c414ff249b36ff20d2090c538e5346c97c0cb3
Cassyyyzxc <164591955+Cassyyyzxc@users.noreply.github.com>
2024-04-06 23:45:43 +0800

    Update AdoptAPet.java

--- src/com/app/view/AdoptAPet.java
+++ src/com/app/view/AdoptAPet.java
@@ -1,13 +1,33 @@
-
+/**
+ * The AdoptAPet class is represents the view for adopting a pet.
+ * It stores functionality to show options for aopting a pet, including
+ * pendiang adoptions and available pet types.
+ *
+ * This class by the user interface to accelerate pet adoptions functionality.
+ * It interconnect with ADPending and ADPetTypes classes.
+ *
+ * @author Alessa Estaras, Cassidy Fernandez, Kapangyarihan Randy, Marc King, Jhanna Llovit
+ *
+ * @version 04/06/2023
+ */
 package com.app.view;
 
 import com.app.model.Account;
 import com.app.model.Pets;
 import java.util.Scanner;
 
+/**
+ * Showing choices for adopting a pet and holds user input
+ * @param account The object representing the user account
+ */
 public class AdoptAPet {
     public void adoptAPet(Account account) { // Page for Adopter
         char choice;
+
+        /*
+	     Scanner method named in to get the input from the user
+	     to be used all throughout the program
+	    */
         Scanner sc = new Scanner(System.in);
         ADPendingAdoptions aa = new ADPendingAdoptions();
         ADPetTypes ap = new ADPetTypes();
