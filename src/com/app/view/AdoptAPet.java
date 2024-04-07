9f212fc7ac86f4ca93c29fd71dd906d685f88002
Cassyyyzxc <164591955+Cassyyyzxc@users.noreply.github.com>
2024-04-08 00:28:37 +0800

    Update AdoptAPet.java

--- src/com/app/view/AdoptAPet.java
+++ src/com/app/view/AdoptAPet.java
@@ -1,4 +1,15 @@
-
+/**
+ * The AdoptAPet class is represents the view for adopting a pet.
+ * It stores functionality to show options for aopting a pet, including
+ * pendiang adoptions and available pet types.
+ *
+ * This class by the user interface to accelerate pet adoptions functionality.
+ * It interconnect with ADPending and ADPetTypes classes.
+ *
+ * @author Alessa Estaras, Cassidy Fernandez, Kapangyarihan Randy, Marc King, Jhanna Llovit
+ *
+ * @version 04/06/2023
+ */
 package com.app.view;
 
 import com.app.model.Account;
@@ -6,8 +17,18 @@ import com.app.model.Pets;
 import java.util.Scanner;
 
 public class AdoptAPet {
+
+   /**
+    * Showing choices for adopting a pet and holds user input
+    * @param account The object representing the user account
+    */
     public void adoptAPet(Account account) { // Page for Adopter
         char choice;
+
+        /*
+	     Scanner method named in to get the input from the user
+	     to be used all throughout the program
+	    */
         Scanner sc = new Scanner(System.in);
         ADPendingAdoptions aa = new ADPendingAdoptions();
         ADPetTypes ap = new ADPetTypes();
@@ -23,13 +44,13 @@ public class AdoptAPet {
             pet.setAdopter_id(account.getUser_id()); // Sets pet's adopter_id with users_id
             switch (choice) {
                 case '1':
-                    aa.adPendingAdoption(pet,account);  //Calls method of class ADPendingAdoption
+                    aa.adPendingAdoption(pet,account);  // Calls method of class ADPendingAdoption
                     continue;
                 case '2':
-                    ap.adPetTypes(account); //Calls method of class ADPetTypes
+                    ap.adPetTypes(account); // Calls method of class ADPetTypes
                     continue;
                 case 'B':
-                    cr.chooseARole(account);
+                    cr.chooseARole(account); // Go Back
                 default: 
                     System.out.println("");
                     System.out.println("Invalid Input");
