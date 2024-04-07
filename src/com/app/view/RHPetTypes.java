d9a93a4111696344a777ad0f56ec5c7a9afbd9ce
Cassyyyzxc <164591955+Cassyyyzxc@users.noreply.github.com>
2024-04-08 00:59:58 +0800

    Update RHPetTypes.java

--- src/com/app/view/RHPetTypes.java
+++ src/com/app/view/RHPetTypes.java
@@ -1,4 +1,13 @@
-
+/**
+ * The RHPetTypes class provides selection for choosing pet types during the rehoming.
+ * It allows user to select from different types of a pets.
+ *
+ * This class impart methods to show the available pet types menu, take user input and proceed.
+ *
+ * @author Alessa Estaras, Cassidy Fernandez, Kapangyarihan Randy, Marc King, Jhanna Llovit
+ *
+ * @version 04/08/2023
+ */
 package com.app.view;
 
 import com.app.model.Account;
@@ -6,6 +15,12 @@ import com.app.model.Pets;
 import java.util.Scanner;
 
 public class RHPetTypes {
+
+    /**
+     * Showing the available pet types menu and allows users to select a pet types they want or go back.
+     * @param account The object represents the user.
+     * @param pet The object represents the pet being rehomed.
+     */
     public void rhPetTypes(Account account, Pets pet){
         RehomeAPet rap = new RehomeAPet();
         Scanner sc = new Scanner(System.in);
