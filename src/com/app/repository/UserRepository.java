4943628e69f1f2536d518bd6a934486f86006c7a
Alessa Estaras <115864832+alessaest@users.noreply.github.com>
2024-04-08 00:15:05 +0800

    Update UserRepository.java

--- src/com/app/repository/UserRepository.java
+++ src/com/app/repository/UserRepository.java
@@ -1,8 +1,34 @@
+/**
+ * The UserRepository interface defines methods for user-related operations.
+ *
+ * @author Alessa Estaras, Cassidy Fernandez, Randy Kapangyarihan, Marc King, Jhanna Llovit
+ *
+ * @version 04/08/2024
+ */
 package com.app.repository;
 import com.app.model.Account;
 
 public interface UserRepository {
+
+    /**
+     * Creates a new user account.
+     *
+     * @param account The account to be created.
+     */
     void createAccount (Account account);
+
+    /**
+     * Logs in a user account.
+     *
+     * @param account The account to log in.
+     */
     void logInAccount (Account account);
+
+    /**
+     * Updates user account details based on the choice provided.
+     *
+     * @param account The account to be updated.
+     * @param choice The choice representing the type of update.
+     */
     public void updateAccount (Account account, int choice);
 }
