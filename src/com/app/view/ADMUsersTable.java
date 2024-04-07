417e2686f9008d7c43d1e38ea8f10d7c528f6e77
Alessa Estaras <115864832+alessaest@users.noreply.github.com>
2024-04-08 00:47:13 +0800

    Update ADMUsersTable.java

--- src/com/app/view/ADMUsersTable.java
+++ src/com/app/view/ADMUsersTable.java
@@ -1,3 +1,11 @@
+/**
+ * The ADMUsersTable class handles displaying the user accounts table.
+ * This class is responsible for displaying the complete details of the users
+ * 
+ * @author Alessa Estaras, Cassidy Fernandez, Randy Kapangyarihan, Marc King, Jhanna Llovit
+ *
+ * @version 04/08/2024
+ */
 package com.app.view;
 
 import java.util.ArrayList;
@@ -5,6 +13,10 @@ import com.app.controller.AdminController;
 import com.app.model.Account;
 
 public class ADMUsersTable {
+
+    /**
+     * Displays the user accounts table.
+     */
     public void admUsersTable() {
         try {
             // Retrieve account list from controller
