28358143de9517ef93ba5928cfe6abb33566ef8c
Cassyyyzxc <164591955+Cassyyyzxc@users.noreply.github.com>
2024-04-08 00:25:49 +0800

    Update ChooseARole.java

--- src/com/app/view/ChooseARole.java
+++ src/com/app/view/ChooseARole.java
@@ -1,4 +1,16 @@
-
+/**
+ * The ChooseARole class represents the view for selecting role
+ * It dispense functionality to display options for a role, including
+ * Adopting a pet or Rehoming a pet
+ * This class impart methods to access and adapt account attributes.
+ *
+ * This class is used by the user interface to accelerate role selections functionality
+ * It interconnects with UserController class to updates user account based on the role
+ *
+ * @author Alessa Estaras, Cassidy Fernandez, Kapangyarihan Randy, Marc King, Jhanna Llovit
+ *
+ * @version 04/06/2023
+ */
 package com.app.view;
 
 import com.app.controller.UserController;
@@ -6,8 +18,19 @@ import com.app.model.Account;
 import java.util.Scanner;
 
 public class ChooseARole {
-
+        
+       /**
+        * Showing choices for choosing a role and holds user input.
+        * Updating the account of the user based on the role
+        *
+        * @param account The object representing the user account
+        */
         public void chooseARole (Account account) {
+                
+        /*
+         Scanner method named in to get the input from the user
+         to be used all throughout the program
+	*/       
         Scanner sc = new Scanner(System.in);
         UserController uc = new UserController();
         
