1b1419672c430f667000ce234c4d14bc6268c621
Cassyyyzxc <164591955+Cassyyyzxc@users.noreply.github.com>
2024-04-08 00:42:58 +0800

    Update ADMDeletePets.java

--- src/com/app/view/ADMDeletePets.java
+++ src/com/app/view/ADMDeletePets.java
@@ -1,11 +1,22 @@
-
+/**
+ * The ADMDeletePets class allows admin to delete pets from the database.
+ * Admin can select a pet from the showed list and delete it.
+ *
+ * This class impart methods to prompt the user to choose a pet for deletion
+ * After deletion, it updates.
+ *
+ * @author Alessa Estaras, Cassidy Fernandez, Kapangyarihan Randy, Marc King, Jhanna Llovit
+ *
+ * @version 04/08/2023
+ */
 package com.app.view;
 
 import com.app.controller.AdminController;
 import java.util.Scanner;
 
 public class ADMDeletePets {
-    
+
+    // Prompts the user to choose a pet for deletion and updates the pets table.
     public void admDeletePets(){
         
         ADMChooseATable adct = new ADMChooseATable();
