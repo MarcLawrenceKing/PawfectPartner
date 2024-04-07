2dc251ae54665363b86754e44206ac92b051866c
Alessa Estaras <115864832+alessaest@users.noreply.github.com>
2024-04-08 00:11:20 +0800

    Update RehomeRepository.java

--- src/com/app/repository/RehomeRepository.java
+++ src/com/app/repository/RehomeRepository.java
@@ -1,4 +1,10 @@
-
+/**
+ * The RehomeRepository interface defines methods for rehoming-related operations.
+ *
+ * @author Alessa Estaras, Cassidy Fernandez, Randy Kapangyarihan, Marc King, Jhanna Llovit
+ *
+ * @version 04/06/2024
+ */
 package com.app.repository;
 
 import java.util.ArrayList;
@@ -6,10 +12,41 @@ import java.util.ArrayList;
 import com.app.model.Pets;
 
 public interface RehomeRepository {
+
+    /**
+     * Retrieves a list of pets pending adoption for a specific pet.
+     *
+     * @param pet The pet for which pending adoptions need to be retrieved.
+     * @return An ArrayList of Pets objects representing pending adoptions.
+     */
     public ArrayList<Pets> rhPendingAdoptions(Pets pet);
-    public void RHUpdateToAPPROVED(Pets pet);
+
+    /**
+     * Updates the status of a pet to "APPROVED" for successful adoptions.
+     *
+     * @param pet The pet for which the status needs to be updated.
+     */
+    public void RHUpdateToAPPROVED(Pets pet); 
+
+    /**
+     * Updates the status of a pet to "NOT APPROVED" for unsuccessful adoptions.
+     *
+     * @param pet The pet for which the status needs to be updated.
+     */
     public void RHUpdateToNOTAPPROVED(Pets pet);
-    public void RHUpdateToARCHIVED(Pets pet);    
+
+    /**
+     * Updates the status of a pet to "ARCHIVED" for completed adoptions.
+     *
+     * @param pet The pet for which the status needs to be updated.
+     */
+    public void RHUpdateToARCHIVED(Pets pet); 
+
+    /**
+     * Creates profiles for pets available for adoption.
+     *
+     * @param pet The pet for which the profile needs to be created.
+     */
     public void rhPetProfiles(Pets pet);
 
 }
