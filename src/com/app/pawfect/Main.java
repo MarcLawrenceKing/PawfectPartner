0801e55df4a71e2170d0e61c8360135b768e3c35
Alessa Estaras <115864832+alessaest@users.noreply.github.com>
2024-04-07 23:53:17 +0800

    Update Main.java

--- src/com/app/pawfect/Main.java
+++ src/com/app/pawfect/Main.java
@@ -1,11 +1,20 @@
+/**
+ * The Main class contains the entry point for the application.
 
+ * @author Alessa Estaras, Cassidy Fernandez, Randy Kapangyarihan, Marc King, Jhanna Llovit
+
+ * @version 04/07/2024
+ */
 package com.app.pawfect;
 
 import com.app.view.DisplayHomePage;
 
+/**
+ * Main method to start the system. Calling and displaying the DisplayHomePage.
+*/
 public class Main {
     public static void main(String[] args) {    
-        DisplayHomePage dh = new DisplayHomePage();
+        DisplayHomePage dh = new DisplayHomePage(); 
         
         dh.displayHomePage();
 
