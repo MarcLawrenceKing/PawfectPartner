641dec322a1f1f3fe098a3693965fac53d70a95d
Alessa Estaras <115864832+alessaest@users.noreply.github.com>
2024-04-07 23:59:59 +0800

    Update AdminRepository.java

--- src/com/app/repository/AdminRepository.java
+++ src/com/app/repository/AdminRepository.java
@@ -1,3 +1,10 @@
+/**
+ * The AdminRepository interface defines methods for administrative operations.
+ *
+ * @author Alessa Estaras, Cassidy Fernandez, Randy Kapangyarihan, Marc King, Jhanna Llovit
+ *
+ * @version 04/07/2024
+ */
 package com.app.repository;
 
 import java.util.ArrayList;
@@ -6,11 +13,47 @@ import com.app.model.Account;
 import com.app.model.Pets;
 
 public interface AdminRepository {
+
+    /**
+     * Logs in an administrator account.
+     *
+     * @param account The administrator account to log in.
+     */
     public void logInAccountAdmin (Account account);
+
+    /**
+     * Retrieves a list of user accounts.
+     *
+     * @return An ArrayList of Account objects representing user accounts.
+     */
     public ArrayList<Account> admUsersTable();
+
+    /**
+     * Retrieves a list of pets.
+     *
+     * @return An ArrayList of Pets objects representing pets.
+     */
     public ArrayList<Pets> admPetsTable ();
+
+    /**
+     * Retrieves a list of archived pets.
+     *
+     * @return An ArrayList of Pets objects representing archived pets.
+     */
     public ArrayList<Pets> admArchivedPetsTable ();
+
+    /**
+     * Deletes a pet from the database.
+     *
+     * @param choice The ID of the pet to delete.
+     */
     public void admDeletePets(int choice);
+
+    /**
+     * Retrieves an archived pet from the database and restores it.
+     *
+     * @param choice The ID of the pet to restore.
+     */
     public void admRetrievePetsArchived (int choice);
 
 }
